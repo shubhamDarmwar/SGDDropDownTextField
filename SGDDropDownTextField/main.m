43b7f76e507875a0afac6f962847e7943cd96e6c
@Laxman Murugappan <mlaxman.m@gmail.com>
2016-06-10 15:26:05 +0530

    Initial Commit

---
+++ SGDDropDownTextField/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SGDDropDownTextField
+//
+//  Created by Laxman Murugappan on 6/10/16.
+//  Copyright © 2016 Shubham Daramwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
